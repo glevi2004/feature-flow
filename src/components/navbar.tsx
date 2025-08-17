ac7ffae4ab916d4fa08fd454f2e44840c56221e1
Levi Ramos <leviramosgabriel2021@gmail.com>
2025-08-17 20:16:25 -0300

    fixed login button hover

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -43,7 +43,7 @@ export function Navbar() {
             <Link href="/login">
               <Button
                 variant="ghost"
-                className="text-white hover:text-white/90 hover:bg-white/5 px-6 py-2 transition-all duration-200"
+                className="text-white hover:!bg-white/10 hover:!text-white/90 px-6 py-2 transition-all duration-200"
               >
                 Login
               </Button>
